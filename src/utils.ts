f7860bb6591cd2a55ee295101691013c0a033b81
Muppet <falconlu2000@gmail.com>
2022-02-19 17:12:15 +0100

    Refactor

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,7 @@ export function arbitrary(array: Array<string>) {
 export function collect_variables(expression: string) {
   return expression.match(/[a-z_]\w*(?!\w*\s*\()/ig);
 }
+// deno-lint-ignore no-explicit-any
 export function arb_set(set: Set<any>) { //skipcq: JS-0323
   for (const e of set) {
     return e;
