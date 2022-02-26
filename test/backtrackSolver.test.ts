06a5784e041d2ba94613e6e110a0b6c2bfd3fc0c
TheMuppet <72599906+TheMuppet@users.noreply.github.com>
2022-02-26 19:28:54 +0100

    Implement Backtrack Solver (#11)
    * Implement Brute Force Solver

---
+++ test/backtrackSolver.test.ts
@@ -0,0 +1,56 @@
+import { assert } from "../deps.ts";
+import { nQueensProblemCSP } from "../src/example-CSPs/nQueensProblem.ts";
+import { allSolProc } from "../src/solutionProcessors.ts";
+import { isConsistent, solve } from "../src/solver/backtrackSolver.ts";
+import { checkAllConstraints } from "../src/solver/bruteForceSolver.ts";
+import { getCSPwithVars } from "../src/utils.ts";
+
+Deno.test({
+  name: "Test Brute Force Solver on 8-Queens Problem ",
+  fn: () => {
+    const n = 8;
+    const csp = nQueensProblemCSP(n);
+    const sol = solve(csp);
+    //console.log(sol);
+    if (sol) {
+      assert(checkAllConstraints(sol, csp.constraints));
+    } else {
+      assert(false);
+    }
+  },
+});
+Deno.test({
+  name: "Test Brute Force Solver on 8-Queens Problem with All Solution",
+  fn: () => {
+    const n = 8;
+    const solProc = allSolProc;
+    const csp = nQueensProblemCSP(n);
+    solve(csp, solProc);
+    //console.log(solProc.allSolutions)
+    if (solProc.allSolutions) {
+      assert(solProc.allSolutions.size == 92);
+    } else {
+      assert(false);
+    }
+  },
+});
+
+Deno.test({
+  name: "Test Is Consistent Positive",
+  fn: () => {
+    const cons = getCSPwithVars(nQueensProblemCSP(4)).constraints;
+    const assignment = { "V1": 2, "V2": 4, "V3": 1 };
+    const result = isConsistent("V4", 3, assignment, cons);
+    assert(result);
+  },
+});
+
+Deno.test({
+  name: "Test Is Consistent Negativ",
+  fn: () => {
+    const cons = getCSPwithVars(nQueensProblemCSP(4)).constraints;
+    const assignment = { "V1": 2 };
+    const result = isConsistent("V2", 2, assignment, cons);
+    assert(!result);
+  },
+});
